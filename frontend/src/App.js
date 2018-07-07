f8fb31bc662319b1a4b5f3bfa68f454a0f9fa2ad
Thomas Ladd <thomas.ladd@stackpath.com>
2018-07-07 17:16:10 -0500

    Add base todo app code Add graphql package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import React, { Component } from "react";
+
+const TodoItem = ({ onDestroy, todo }) => {
+  return (
+    <li>
+      <div className="view">
+        <label>{todo.title}</label>
+        <button className="destroy" onClick={onDestroy} />
       </div>
+    </li>
+  );
+};
+
+type AppState = {
+  newTodo: string
+};
+
+class App extends Component<{}, AppState> {
+  state = {
+    newTodo: ""
+  };
+
+  render() {
+    const todosYo = [
+      { id: 1, title: "Title 1" },
+      { id: 2, title: "Title 2" },
+      { id: 3, title: "Title 3" }
+    ];
+    let main = todosYo.length ? (
+      <section className="main">
+        <ul className="todo-list">
+          {todosYo.map(todo => (
+            <TodoItem key={todo.id} onDestroy={() => {}} todo={todo} />
+          ))}
+        </ul>
+      </section>
+    ) : null;
+
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            value={this.state.newTodo}
+            onKeyDown={e => {
+              if (e.keyCode !== 13) {
+                return;
+              }
+              e.preventDefault();
+
+              var val = this.state.newTodo.trim();
+
+              if (val) {
+                this.setState({ newTodo: "" });
+              }
+            }}
+            onChange={e => this.setState({ newTodo: e.target.value })}
+            autoFocus={true}
+          />
+        </header>
+        {main}
+      </section>
     );
   }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// @flow
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "todomvc-common/base.css";
+import "todomvc-app-css/index.css";
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.render(<App />, rootElement);
+}
